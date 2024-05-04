f882d9f460e16e3dfaba768f242c9cad4e0da8d3
anurup-joseph <37307041+anurup-joseph@users.noreply.github.com>
2024-05-04 18:35:58 +0000

    Checking in work in progress.

--- src/_02_code_flow/_4_flaming_ninja_star/FlamingNinjaStar.java
+++ src/_02_code_flow/_4_flaming_ninja_star/FlamingNinjaStar.java
@@ -5,48 +5,65 @@ import java.awt.Color;
 
 public class FlamingNinjaStar {
 	public static void main(String[] args) {
+		int baseSize = 200; // the size of the black part of the star
+		int flameSize = 130; // the length of the flaming arms
 
-		int baseSize = 200;		//the size of the black part of the star
-		int flameSize = 130;		//the length of the flaming arms
-		
 		// Make a new robot, and set it's pen down.
+		Robot rob = new Robot();
+		rob.penDown();
 
 		// Set the robot speed to 100
-		
+		rob.setSpeed(100);
+
 		// Set the robot window size to 800 x 800
+		rob.setWindowSize(800, 800);
 
-		//  COUNT. Make another int variable to count how many times the loop has repeated
-		//         Set its start value to zero.
-		
-	       //  LOOP. Start a while loop to repeat all of the code below ONE time (we will change this later)
+		// COUNT. Make another int variable to count how many times the loop has
+		// repeated
+		// Set its start value to zero.
+		int count = 0;
 
-			   // TURN RIGHT     Turn the robot 1/8 of a circle (hint: 360 degrees will turn a full circle)
-		
-			   // MOVE           Move the robot 64 pixels
+		// LOOP. Start a while loop to repeat all of the code below ONE time (we will
+		// change this later)
+		while (count < 1) {
+			// TURN RIGHT Turn the robot 1/8 of a circle (hint: 360 degrees will turn a full
+			// circle)
+			rob.turn(360 / 8);
 
-			   // TURN LEFT      Turn the robot 40 degrees to the LEFT. (Negative numbers will turn the robot counter-clockwise.)
-			
-			   // DRAW FLAME     Move the robot the distance in the variable flameSize
-			
-				//               Turn the robot 170 degrees
-			
-				//               Move the robot the distance in the variable flameSize (again)
-			
-			   // TURN RIGHT     Turn the robot 64 degrees to the right
-			
-				// MOVE         Move the robot the distance in the variable baseSize
-		
-				//  INCREASE COUNT. Increase the count by 1
-		
-		//  End the while loop here
-		
-		// TEST   Run the program. Check that your shape is the same as the first picture in the recipe. 
-		//        This is one arm of the ninja star.
-		
-		// COLOR  Change the Robot's pen color so that the flame is a different color to the rest of the star.
-		//        Run the program again. Check the second picture in the recipe.
+			// MOVE Move the robot 64 pixels
+			rob.move(64);
 
-		// LOOP   When you have one arm looking right, change your loop to repeat 25 times and run the program.
-		
+			// TURN LEFT Turn the robot 40 degrees to the LEFT. (Negative numbers will turn
+			// the robot counter-clockwise.)
+			rob.turn(-40);
+
+			// DRAW FLAME Move the robot the distance in the variable flameSize
+			rob.move(flameSize);
+
+			// Turn the robot 170 degrees
+
+			// Move the robot the distance in the variable flameSize (again)
+
+			// TURN RIGHT Turn the robot 64 degrees to the right
+
+			// MOVE Move the robot the distance in the variable baseSize
+
+			// INCREASE COUNT. Increase the count by 1
+
+			// End the while loop here
+
+		}
+
+		// TEST Run the program. Check that your shape is the same as the first picture
+		// in the recipe.
+		// This is one arm of the ninja star.
+
+		// COLOR Change the Robot's pen color so that the flame is a different color to
+		// the rest of the star.
+		// Run the program again. Check the second picture in the recipe.
+
+		// LOOP When you have one arm looking right, change your loop to repeat 25 times
+		// and run the program.
+
 	}
 }
